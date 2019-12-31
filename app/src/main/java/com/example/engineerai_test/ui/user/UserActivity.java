8d57b047175adc37dc87676a4772850f89258f78
Digvesh <digvesh.patel@tatvasoft.com>
2019-12-31 10:07:25 +0530

    UI For List and row Reflector changes

---
+++ app/src/main/java/com/example/engineerai_test/ui/user/UserActivity.java
@@ -0,0 +1,17 @@
+package com.example.engineerai_test.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.engineerai_test.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        //"http://sd2-hiring.herokuapp.com/api/";
+    }
+}
